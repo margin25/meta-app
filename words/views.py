9a530e3a5c2a7a8ec1e19d65d48753f409a28f8f
dylanheathcock <dylanryanheathcock@gmail.com>
2022-05-02 17:32:59 -0500

    Changed Main API for definitions and fixed error handling

--- words/views.py
+++ words/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 import requests
 from django.shortcuts import render
 import _json
@@ -49,37 +50,31 @@ def homepage(request):
     return render(request, 'homepage.html', {"word" : wordOfDay, "definition" : definition})
 
 def wordpage(request, word):
-    #get api request url
-    url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"
-    key = "?key=d7045d21-8d0c-4c61-9aea-8321bb66b12e"
-    url += word + key
+    #WORDS API DEFINITION
+    url = "https://wordsapiv1.p.rapidapi.com/words/"
+    url += word + "/definitions"
 
-    #get json file from api
-    response = requests.get(url)
-    wordJson = response.json()
+    headers = {
+        "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+        "X-RapidAPI-Key": "bdf46a7a41msh7340c999b57f051p1cdf9cjsn032baa9651fa"
+    }
+
+    formalDefList = []
+
     try:
-       wordJson[0]['def'] 
+        response = requests.request("GET", url, headers=headers)
+        wordJson = response.json()
+        i = 1
+        for d in (wordJson['definitions']):
+            x = str(i) + ": " + str(d['definition'])
+            formalDefList.append(x)
+            i += 1
     except:
-        return render(request, 'notword.html')
+        return render(request, "notword.html")
 
-    definition =   ((((((wordJson[0]['def'])[0])['sseq'])[0])[0])[0])
-    if (definition == "pseq"):
-        definition = (((((((wordJson[0]['def'])[0])['sseq'])[0])[0])[1])[0][1])['dt'][0][1]
-    else:
-        definition = (    ((((((((wordJson[0]['def'])[0])['sseq'])[0])[0])[1])['dt'])[0])[1]   )
-    definition = definition[4:]
 
-    if (definition.find("{it}")):
-        definition = definition.replace("{it}","")
-        definition = definition.replace("{/it}","")
 
-    while (definition.find("{d_link|") != -1):
-        definition = definition.replace("{d_link|","",1)
-        removeStart = definition.find("|")
-        removeEnd = definition.find("}", removeStart)
-        definition = definition[0:removeStart] + definition[removeEnd + 1:]
-
-    #Urban Dictionary API
+    #Urban Dictionary API Dictionary
 
     #Send API request and get response
     url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
@@ -87,20 +82,28 @@ def wordpage(request, word):
     headers = {
     'x-rapidapi-key': "bdf46a7a41msh7340c999b57f051p1cdf9cjsn032baa9651fa",
     'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com"
-    }
+    }   
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     wordJson = response.json()
+
 
     #Parse through json and clean up
     slangDef = ((wordJson['list'])[0])['definition']
     slangDef = slangDef.replace("[","")
     slangDef = slangDef.replace("]","")
 
-    definition = str(definition)
     slangDef = str(slangDef)
+
+    #define the dictionary to front end
+    context = {
+        "word" : word,
+        "Formal" : formalDefList,
+        "slangDef" : slangDef
+    }
+
     #send to front end
-    return render(request, 'wordpage.html', {"word" : word, "definition": definition, "slangDef" : slangDef})
+    return render(request, 'wordpage.html', context)
 
 def get_search(request):
     # if this is a POST request we need to process the form data
@@ -113,6 +116,8 @@ def get_search(request):
             wordSearch = form.cleaned_data['searchbar']
             # redirect to a new URL:
             return HttpResponseRedirect('/' + wordSearch)
+        else:
+            return render(request, 'notword.html')
 
     # if a GET (or any other method) we'll create a blank form
     else:
